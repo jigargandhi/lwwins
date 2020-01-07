1221ad64a7366b58acea62f48bb4d21004971246
jigargandhi <jigargandhi>
2020-01-07 22:06:52 +0530

    Fixed address_test.go

--- address/address_test.go
+++ address/address_test.go
@@ -10,15 +10,14 @@ func TestAddressInitialize(t *testing.T) {
 	assert.NotNil(t, registrar, "Make should not return nil")
 }
 
-func TestAddressHandler_WhenGivenCorrectAddress_AddsAndNotifiesChannel(t *testing.T) {
-	t.Skip("test skipped until I learn")
+func TestAddressHandler(t *testing.T) {
 	registrar := Make(1, "token")
-	handleAddress("2|token|171.0.0.0", registrar)
-	val, ok := registrar.address[2]
 	go func() {
 		<-registrar.NewAddress
 	}()
+	handleAddress("2|token|171.0.0.0", registrar)
+
+	val, ok := registrar.address[2]
 	assert.True(t, ok, "registrar contains a new address")
 	assert.Equal(t, "171.0.0.0", val, "expected 172.0.0.0 go %v ", val)
-
 }
